1fe7480e442ea5f9c47789b523fc3114fa3323db
Nathan Bland <nathan.bland@gmail.com>
2018-11-27 20:11:24 -0700

    Add filtering to item and user

--- routes/item/read.js
+++ routes/item/read.js
@@ -4,18 +4,33 @@ const Item = require('../../models/Item')
 router.get('/', (req, res, next) => {
   const pageSize = 20
   const currentPage = req.query.page > 0 ? req.query.page - 1 : 0
+  const filter = req.query.filter || ''
+  const filterOn = req.query.filterOn || ''
   const sortBy = req.query.sortBy || 'createdAt'
   const orderBy = req.query.orderBy || 'asc'
   const sortQuery = {
     [sortBy]: orderBy
   }
+  let filterQuery = {}
+  if (filter.length > 0) {
+    const regx = new RegExp(filter, 'i')
+    if (filterOn.length > 0) {
+      filterQuery = {
+        [filterOn]: regx
+      }
+    } else {
+      filterQuery = {
+        content: regx
+      }
+    }
+  } 
 
-  Item.count()
+  Item.countDocuments(filterQuery)
   .then(itemCount => {
     if (currentPage * pageSize > itemCount) {
       return res.status(400).json([])
     }
-    Item.find()
+    Item.find(filterQuery)
     .limit(pageSize)
     .skip(currentPage * pageSize)
     .sort(sortQuery)
@@ -28,7 +43,10 @@ router.get('/', (req, res, next) => {
       })
     })
   })
-
+  .catch(err => {
+    console.log('Error finding item:', err)
+    return res.status(500).json({msg: 'no item found'})
+  })
 })
 
 router.get('/:id', (req, res, next) => {
--- routes/user/read.js
+++ routes/user/read.js
@@ -4,18 +4,22 @@ const User = require('../../models/User')
 router.get('/', (req, res, next) => {
   const pageSize = 20
   const currentPage = req.query.page > 0 ? req.query.page - 1 : 0
+  const filter = req.query.filter || ''
   const sortBy = req.query.sortBy || 'username'
   const orderBy = req.query.orderBy || 'asc'
   const sortQuery = {
     [sortBy]: orderBy
   }
+  const filterQuery = {
+    username: new RegExp(filter, 'i')
+  }
 
-  User.count()
+  User.countDocuments(filterQuery)
   .then(userCount => {
     if (currentPage * pageSize > userCount) {
       return res.status(400).json([])
     }
-    User.find()
+    User.find(filterQuery)
     .limit(pageSize)
     .skip(currentPage * pageSize)
     .sort(sortQuery)
@@ -27,6 +31,10 @@ router.get('/', (req, res, next) => {
         pageSize: pageSize
       })
     })
+  })
+  .catch(err => {
+    console.log('Error finding user:', err)
+    return res.status(500).json({msg: 'no user found'})
   })
 })
 
